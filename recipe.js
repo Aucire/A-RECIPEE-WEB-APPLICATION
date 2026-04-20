9dd466a0452d4323d3f35855b37e7fe5e9dd4b01
Aucire <jamesandaro0@gmail.com>
2026-04-20 23:24:34 +0300

    major changes'dropdown-list'

--- recipe.js
+++ recipe.js
@@ -1,27 +1,71 @@
-// TEST
+
 console.log("Javascript is working");
 // const input="kentucky fried chicken"
 
 //variables to hold Name, Ingredients, Instructions, Searchbar and button
 const containerName=document.getElementById("mealName")
-//containerName.innerHTML = "";
 const containerIngredients=document.getElementById("ingredients")
 const containerInstructions=document.getElementById("instructions")
 const searchInput=document.querySelector(".input")
 const button=document.getElementById("btn")
 
-// OUR MAIN FUNCTION THAT DISPLAYS THE DATA FROM API
-async function getRecipe(foodName) {
+let optionSelected;
+
+
+// Function to deal with multiple Meal Names from the Server
+async function getMealNames(foodName) {
+    try{
+        // Fetching data from the DB Meal Server
+        const baseUrl=`https://www.themealdb.com/api/json/v1/1/search.php?s=${encodeURIComponent(foodName)}`
+        const res=await fetch(baseUrl);       
+        
+        const data=await res.json()        
+        console.log(data); 
+
+        // Loop through meal names and store them into a created <select> element
+        data.meals.forEach(foods=>{
+                const listName = document.createElement("option");  
+                listName.value=foods.strMeal
+                listName.textContent=foods.strMeal
+                containerName.appendChild(listName);
+        })
+        if (data.meals.length === 1) {
+            containerName.value = data.meals[0].strMeal;
+            getRecipe(data.meals[0].strMeal);
+        }
+        // An Event Listener for the <select> options 
+        containerName.addEventListener("change",function(){
+            const selectedMeal=this.value 
+
+            if (selectedMeal !== "") {
+                getRecipe(selectedMeal);
+            }
+        })
+    }
+    catch{
+        // Js Styling of the error induced
+        const eerrors=document.getElementById("error")
+        eerrors.style.color="red";
+        eerrors.style.fontSize = "30px";
+        eerrors.style.fontWeight="bold"
+        eerrors.style.marginLeft = "25px";
+        eerrors.style.fontStyle="italic"
+        //feedback
+        eerrors.textContent = "No recipes found...!";
+        console.log(error);
+    }
+}
+
+//  FUNCTION THAT DISPLAYS THE DATA FROM API
+async function getRecipe(optionSelected) {
     // Having the try...catch to track errors with the API
     try{
-
         // Initially setting the text contents to empty
-        containerName.innerHTML = "";
         containerIngredients.innerHTML = "";
         containerInstructions.innerHTML = "";
 
         // OUR BASE API URL
-        const baseUrl=`https://www.themealdb.com/api/json/v1/1/search.php?s=${encodeURIComponent(foodName)}`
+        const baseUrl=`https://www.themealdb.com/api/json/v1/1/search.php?s=${encodeURIComponent(optionSelected)}`
 
         const res=await fetch(baseUrl);     //getting response from the API    
         //console.log(res);
@@ -29,26 +73,9 @@ async function getRecipe(foodName) {
         const data=await res.json()         //converting response to data
         console.log(data); 
 
-        // A code to check if user input exists
-        if (!data.meals) {
-            // Js Styling
-            containerName.style.color="red";
-            containerName.style.fontSize = "30px";
-            containerName.style.fontWeight="bold"
-            containerName.style.marginLeft = "25px";
-            containerName.style.fontStyle="italic"
-            //feedback
-            containerName.textContent = "No recipes found...!";
-            return;
-        }
 
         // Looping through the meals arrray to locate user input
         data.meals.forEach(item=>{
-            const listName = document.createElement("div");   //a div to hold strname
-            
-            listName.textContent = item.strMeal;
-            containerName.appendChild(listName);     //appending the div to html
-
             //FOR LOOP TO GET INGREDIENTS ONE BY ONE
             for (let i = 1; i <= 20; i++) {
                 const ingredient = item[`strIngredient${i}`];
@@ -68,23 +95,20 @@ async function getRecipe(foodName) {
             const listInstructions = document.createElement("div");
             listInstructions.textContent = item.strInstructions;
             containerInstructions.appendChild(listInstructions);
-
-        }); 
+        });  
     }
     //The catch for returning errors
     catch(err) {
         document.getElementById('error').innerHTML = "Error occurred with your connection...!";
         console.error(err);
     }
-
 }
 
 // AN EVENT LISTENER FOR THE BUTTON AND SEARCHBAR
 button.addEventListener("click",(event)=>{
-    event.preventDefault();                           //prevents defaulst bahaviuor of html form submission
-    getRecipe(searchInput.value)                      //I call the function that takes in user input
-    searchInput.value=""                              //initializing the search bar to empty after submission
+    event.preventDefault();  
+    containerName.innerHTML = "";                       
+    const optionSelected=getMealNames(searchInput.value)                      
+    searchInput.value=""                              
 })
-
-
               //THE END
